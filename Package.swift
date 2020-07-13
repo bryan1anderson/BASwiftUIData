b64a01738d1e69f77ee0479005652265078663c4
Bryan Lloyd Anderson <bryan@dvlop.io>
2020-07-13 11:28:40 -0600

    swift version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BASwiftUIData",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,13 +15,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/bryan1anderson/BAUtilities", .branch("master")),
-    .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.0.0"),
-    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
-    .package(url: "https://github.com/kean/Nuke", from: "9.1.1"),
-    .package(url: "https://github.com/utahiosmac/Marshal", from: "1.2.8"),
-    .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
-        .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.0.0"),
+        .package(name: "BAUtilities", url: "https://github.com/bryan1anderson/BAUtilities", .branch("master")),
+        .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
+        .package(url: "https://github.com/kean/Nuke", from: "9.1.1"),
+        .package(url: "https://github.com/utahiosmac/Marshal", from: "1.2.8"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
+        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", from: "5.0.0"),
         .package(url: "https://github.com/kean/FetchImage", from: "0.2.1")
         
     ],
@@ -29,7 +30,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BASwiftUIData",
-            dependencies: ["PhoneNumberKit", "Alamofire", "Nuke", "Marshal", "EMUtilities", "SwiftyJSON", "Reachability"]),
+            dependencies: ["PhoneNumberKit", "Alamofire", "Nuke", "Marshal", "BAUtilities", "SwiftyJSON", "Reachability", "FetchImage"]),
         .testTarget(
             name: "BASwiftUIDataTests",
             dependencies: ["BASwiftUIData"]),
